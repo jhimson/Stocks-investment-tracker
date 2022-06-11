d6fbadeaa033e1c336dff669f0ab368c41d604cb
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-11 05:33:17 -0400

    Updated routing flow for transactions

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,9 +1,8 @@
 const Transaction = require('../models/transactionsModel');
-
 const showTransactions = async (req, res) => {
   try {
     const transactions = await Transaction.find({});
-    res.render('/stocks/index', {transactions});
+    res.render('transactions/index', { transactions });
   } catch (error) {
     res.json({ message: error.message });
   }
--- routes/transactionsRoutes.js
+++ routes/transactionsRoutes.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { showTransactions } = require('../controllers/transactionsController');
 
-router.get('/', (req, res) => {
-res.render('transactions/index')
-});
+router.get('/', showTransactions);
 
 module.exports = router;
