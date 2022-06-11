8b47ed09858b7b3b21b8705d53c176636ceb4547
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-11 04:41:19 -0400

    Bug fix: transactions seed file not populating the database

--- seed/transactions.js
+++ seed/transactions.js
@@ -1,45 +1,44 @@
 const mongoose = require('../config/database');
-const Transaction = require('../models/transactionsModel')
+const Transaction = require('../models/transactionsModel');
 
 //? save the connection in a variable
 const db = mongoose.connection;
 
 //! Make sure code is not run till connected
 db.on('open', async () => {
-
   const transactions = [
-    { 
-        type: 'Buy', 
-        tickerSymbol: 'MARA',
-        quantity: 200,
-        stockPrice: 7.88,
+    {
+      type: 'Buy',
+      tickerSymbol: 'MARA',
+      quantity: 200,
+      stockPrice: 7.88,
     },
-    { 
-        type: 'Buy', 
-        tickerSymbol: 'AAPL',
-        quantity: 10,
-        stockPrice: 144,
+    {
+      type: 'Buy',
+      tickerSymbol: 'AAPL',
+      quantity: 10,
+      stockPrice: 144,
     },
-    { 
-        type: 'Sell', 
-        tickerSymbol: 'NFLX',
-        quantity: 150,
-        stockPrice: 188,
+    {
+      type: 'Sell',
+      tickerSymbol: 'NFLX',
+      quantity: 150,
+      stockPrice: 188,
     },
   ];
 
   try {
     await Transaction.deleteMany({});
-        try {
-            Transaction.create(transactions);
-            db.close();
-        } catch (error) {
-            res.status(500).json({message: error.message})
-            db.close();
-        }
+    try {
+      const newTransactions = await Transaction.create(transactions);
+      console.log(newTransactions);
+      db.close();
+    } catch (error) {
+      console.log(error);
+      db.close();
+    }
   } catch (error) {
-    res.status(500).json({message: error.message});
+    console.log(error);
     db.close();
   }
-
 });
