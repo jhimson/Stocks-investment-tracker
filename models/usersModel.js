67e8a16e41984441200431c3ad7b31c2e73f3b9d
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-12 08:23:10 -0400

    Created user model

---
+++ models/usersModel.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = model('User', userSchema);
