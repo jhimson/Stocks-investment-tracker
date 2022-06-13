2e007d114bfbb8bf252621368384e8366346520e
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-13 07:14:19 -0400

    Updated watchlists index layout

--- controllers/watchlistsController.js
+++ controllers/watchlistsController.js
@@ -5,7 +5,7 @@ const Watchlist = require('../models/watchlistsModel');
 const getAllWatchlists = async (req, res) => {
   try {
     const watchlists = await Watchlist.find({});
-    res.render('watchlists/index', { watchlists });
+    res.render('watchlists/index', { watchlists, username: req.session.username });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
