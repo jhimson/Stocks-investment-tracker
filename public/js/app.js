e15cad97fd7ef5c122d37246b44d2f7ad06dc892
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-13 12:01:18 -0400

    Implemented update watchlist modal to watchlist card component

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,23 @@
-const newWatchlist = document.getElementById('newWatchlist')
+window.addEventListener('DOMContentLoaded', () => {
+  const overlay = document.querySelector('#overlay');
+  const delBtn = document.querySelector('#delete-btn');
+  const closeBtn = document.querySelector('#close-btn');
+  const cancelBtn = document.querySelector('#cancelBtn');
+  updateWatchlist = document.querySelector('#updateWatchlist');
 
-newWatchlist.addEventListener('click', (e) => {
-    console.log('WTF')
-})
+  const toggleModal = () => {
+    overlay.classList.toggle('hidden');
+    overlay.classList.toggle('flex');
+  };
+
+  updateWatchlist.addEventListener('click', () => {
+    overlay.classList.remove('hidden')
+  })
+
+  cancelBtn.addEventListener('click', () => {
+    overlay.classList.add('hidden')
+  })
+//   delBtn.addEventListener('click', toggleModal);
+//   closeBtn.addEventListener('click', toggleModal);
+//   cancelBtn.addEventListener('click', toggleModal);
+});
