86e6a91e9adda9715014fd8eb5d263c76bf0c77f
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-13 21:05:52 -0400

    Updated stock schema

--- models/stockSchema.js
+++ models/stockSchema.js
@@ -1,18 +1,45 @@
 const mongoose = require('mongoose');
 const { Schema, model } = mongoose;
 
-const stockSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const stockSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    symbol: {
+      type: String,
+      required: true,
+    },
+    logo: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    sector: {
+      type: String,
+    },
+    industry: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    marketCap: {
+      type: Number,
+    },
+    earningsPerShare: {
+      type: Number,
+    },
+    fiftyTwoWeekHigh: {
+      type: Number,
+    },
+    fiftyTwoWeekLow: {
+      type: Number,
+    },
   },
-  symbol: {
-    type: String,
-    required: true,
-  },
-  logo: {
-    type: String
-  }
-});
+  { timestamps: true }
+);
 
 module.exports = model('Stock', stockSchema);
