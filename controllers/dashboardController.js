8d0248d3e0de249b737f2635620c0197d2f8aa63
jhimson <jhimsonrayraboypamisa@gmail.com>
2022-06-14 13:23:14 -0400

    Added queries to get watchlist and transaction counts to feed the dashboard page

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,8 +1,21 @@
 const Watchlist = require('../models/watchlistsModel');
 const Transaction = require('../models/transactionsModel');
 
-const renderDashboardPage = (req, res) => {
-  res.render('dashboard/index', { username: req.session.username });
+const renderDashboardPage = async (req, res) => {
+  try {
+    const watchlistsCount = await Watchlist.find({ user: req.session._id }).count()
+    const transactionsCount = await Transaction.find({
+      user: req.session._id,
+    }).count()
+
+    res.render('dashboard/index', {
+      username: req.session.username,
+      totalWatchlists: watchlistsCount,
+      totalTransactions: transactionsCount,
+    });
+  } catch (error) {
+    res.sendStatus(500).json({ message: error.message });
+  }
 };
 
 module.exports = { renderDashboardPage };
