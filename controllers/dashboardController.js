7a9736a7b7689dc1ddec257a73d525622c4d1bf2
Jhimson Pamisa <jhimsonrayraboypamisa@gmail.com>
2022-06-17 05:36:21 -0400

    Added new query to fetch stocks in DB

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,18 +1,23 @@
 const Watchlist = require('../models/watchlistsModel');
 const Transaction = require('../models/transactionsModel');
+const Stock = require('../models/stockModel');
 
 const renderDashboardPage = async (req, res) => {
   try {
-    const watchlistsCount = await Watchlist.find({ user: req.session._id }).count()
+    const watchlistsCount = await Watchlist.find({
+      user: req.session._id,
+    }).count();
     const transactionsCount = await Transaction.find({
       user: req.session._id,
-    }).count()
+    }).count();
+    const stocks = await Stock.find({}).limit(4);
 
     res.render('dashboard/index', {
       username: req.session.username,
       totalWatchlists: watchlistsCount,
       totalTransactions: transactionsCount,
-      totalAssets: localStorage.getItem('totalAssets')
+      totalAssets: localStorage.getItem('totalAssets'),
+      stocks,
     });
   } catch (error) {
     res.sendStatus(500).json({ message: error.message });
